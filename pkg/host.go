efdcad7f2dd27094ad4369603cce425cc9438d19
hugues <ghugues@netc.fr>
2020-06-11 01:37:47 +0200

    pkg/mail.go: Add message and message.setMeta()

---
+++ pkg/host.go
@@ -0,0 +1,9 @@
+// BSD 3-Clause License
+// Copyright (c) 2020, See AUTHORS file
+// All rights reserved.
+
+package mta
+
+type host struct {
+	name string
+}
---
+++ pkg/mail.go
@@ -0,0 +1,62 @@
+// BSD 3-Clause License
+// Copyright (c) 2020, See AUTHORS file
+// All rights reserved.
+
+package mta
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/mail"
+	"time"
+)
+
+type message struct {
+	host     host
+	to, from string
+	id       string
+
+	content []byte
+}
+
+// Set the date and the Message-Id
+func (m *message) setMeta(r io.Reader) error {
+	now := time.Now()
+	ms, err := mail.ReadMessage(r)
+	if err != nil {
+		return err
+	}
+
+	// Date
+	ms.Header["Date"] = []string{now.Format(time.RFC1123Z)}
+
+	// Message-Id
+	m.id = fmt.Sprintf("%d@%s", now.Unix(), m.host.name)
+	ms.Header["Message-ID"] = []string{m.id}
+
+	// Regenrate the message content.
+	buff := &bytes.Buffer{}
+
+	for n, v := range ms.Header {
+		for _, v := range v {
+			buff.WriteString(n)
+			buff.WriteString(": ")
+			buff.WriteString(v)
+			buff.WriteString("\r\n")
+		}
+	}
+	buff.WriteString("\r\n")
+
+	if _, err = io.Copy(buff, ms.Body); err != nil {
+		return err
+	}
+
+	m.content = buff.Bytes()
+
+	return nil
+}
+
+func (m *message) WriteTo(w io.Writer) error {
+	return nil
+}
